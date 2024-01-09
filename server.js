89fc57f2b7d7bd13058ce2d8b410978ffeccbf65
rbozek <rbozek@gmail.com>
2024-01-09 13:43:39 -0500

    create folder, npm init, type module to json file

---
+++ server.js
@@ -0,0 +1,22 @@
+// import modules
+import express from 'express'
+
+// create Express app
+const app = express()
+
+// configure the app (app.set)
+
+
+// mount Middleware (app.use)
+
+
+// mount routes
+app.get('/', function(req, res) {
+ res.send('<h1>testing main / directory</h1>')
+})
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
