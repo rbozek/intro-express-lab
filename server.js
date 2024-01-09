80e9752ef270d5836f106596c6ac2c68b490771e
rbozek <rbozek@gmail.com>
2024-01-09 13:49:33 -0500

    skip nodemon install, set up another route to icecream

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ const app = express()
 // mount Middleware (app.use)
 
 
-// mount routes
+// Mount routes
 app.get('/', function(req, res) {
- res.send('<h1>testing main / directory</h1>')
-}) 
+ res.send('<h1>Hello Express in main / path</h1>')
+})
+
+app.get('/icecream', function(req, res) {
+ res.send('<h1>Testing routing to ice cream path</h1>')
+})
 
 // tell the app to listen on port 3000
 
