b1b8fb34fa25de13082a3955a3404db4b4e1da69
rbozek <rbozek@gmail.com>
2024-01-09 13:58:46 -0500

    create data folder & file, filled in todos from lesson

---
+++ data/todo-data.js
@@ -0,0 +1,21 @@
+const todos = [
+ {text: 'Feed llama', done: true, _id: 125223},
+ {text: 'Sleep under the stars', done: false, _id: 127904},
+ {text: 'Buy milk', done: false, _id: 139608},
+]
+
+export { 
+todos
+}
+
+// const instruments = [
+//  {text: 'drums', loud: true, easytocarry: false, _id: 100001},
+//  {text: 'bass', loud: false, easytocarry: true, _id: 100002},
+//  {text: 'guitar', loud: true, easytocarry: true, _id: 100003},
+//  {text: 'flute', loud: false, easytocarry: true, _id: 100004},
+//  {text: 'congas', loud: false, easytocarry: false, _id: 100005}
+// ]
+
+// export { 
+// instruments
+// }
